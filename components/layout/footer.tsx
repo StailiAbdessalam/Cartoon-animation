bb405f67331345445f2acf92f7cd6553dfd804c5
BENJY33X <A.STAILI@student.youcode.ma>
2022-07-05 10:52:37 +0100

    footer components

---
+++ components/layout/footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Styles from "../../styles/Home/blog.module.css";
+const footer = () => {
+  return (
+    <footer className={Styles.footer}>
+        Powered by STaili abdessalam didikasse l meknes hh.
+      </footer>
+  )
+}
+
+export default footer
